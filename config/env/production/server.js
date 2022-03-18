49e88a2e5e989442874688aa912958dffc16f56a
freddieswift <freddie9117@live.co.uk>
2022-03-18 13:04:50 +0000

    url update

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-    url: 'https://api.freddieswift.dev',
+    url: env("MY_HEROKU_URL"),
     proxy: true,
     app: {
         keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
